8e312991ffb5e184dacfb0460a94ffa29bbea059
Uroš Jevremović <jevremovic.uros91@gmail.com>
2018-07-06 16:54:02 +0200

    removed duplication and hardcoded links/inputs

--- test_selenium.py
+++ test_selenium.py
@@ -6,32 +6,32 @@ from selenium.webdriver.firefox.webdriver import WebDriver
 class SeleniumTest(TestCase):
     """Testing google.com"""
 
-    def test_python(self):
+    def setUp(self):
         self.browser = WebDriver()
         self.browser.implicitly_wait(5)
         self.browser.get('http://google.com')
+
+    def tearDown(self):
+        self.browser.close()
+
+    def search(self, search_string):
         q = self.browser.find_element_by_name('q')
-        q.send_keys('python')
+        q.send_keys(search_string)
         q.submit()
         results = self.browser.find_element_by_id('search')
-        links = results.find_elements_by_tag_name('a')
-        for link in links:
+        return results.find_elements_by_tag_name('a')
+
+    def test_python(self):
+
+        for link in self.search('python'):
             if 'https://www.python.org/' in (link.get_attribute('href') or ''):
                 break
         else:
             self.fail('python.com is not on google results page')
-        self.browser.close()
 
     def test_python_in_title(self):
-        self.browser = WebDriver()
-        self.browser.implicitly_wait(5)
-        self.browser.get('http://google.com')
-        q = self.browser.find_element_by_name('q')
-        q.send_keys('python')
-        q.submit()
-        self.browser.find_element_by_id('search')
+        self.search('python django')
         assert 'python' in self.browser.title
-        self.browser.close()
 
 
 if __name__ == '__main__':
